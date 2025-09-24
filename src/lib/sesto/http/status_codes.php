a7c664692501925cded2f27ca254ff2232be98cd
Andrea Davanzo <andrea.davanzo@ed.ac.uk>
2025-09-24 13:11:14 +0100

    add SESTO_HTTP_METHOD_NOT_ALLOWED

--- src/lib/sesto/http/status_codes.php
+++ src/lib/sesto/http/status_codes.php
@@ -12,6 +12,7 @@ const SESTO_HTTP_BAD_REQUEST = 400;
 const SESTO_HTTP_UNAUTHORIZED = 401;
 const SESTO_HTTP_FORBIDDEN = 403;
 const SESTO_HTTP_NOT_FOUND = 404;
+const SESTO_HTTP_METHOD_NOT_ALLOWED = 405;
 const SESTO_HTTP_GONE = 410;
 const SESTO_HTTP_INTERNAL_SERVER_ERROR = 500;
 
@@ -24,6 +25,7 @@ function sesto_http_status_codes(): array
     SESTO_HTTP_UNAUTHORIZED => 'Unauthorized',
     SESTO_HTTP_FORBIDDEN => 'Forbidden',
     SESTO_HTTP_NOT_FOUND => 'Not Found',
+    SESTO_HTTP_METHOD_NOT_ALLOWED => 'Method Not Allowed',
     SESTO_HTTP_GONE => 'Gone',
     SESTO_HTTP_INTERNAL_SERVER_ERROR => 'Internal Server Error',
   ];
