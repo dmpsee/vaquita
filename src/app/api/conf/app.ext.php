afc52d0fee3e7db43876f2435ff48cb7a7af7e05
Andrea Davanzo <andrea.davanzo@ed.ac.uk>
2025-09-24 16:18:30 +0100

    remove old file

--- src/app/api/conf/app.ext.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// DMPsee - https://dmpsee.org
-// SPDX-License-Identifier: AGPL-3.0-or-later
-// Copyright Digital Curation Centre (UK) and contributors
-
-$config['sesto_require'][] = SESTO_DIR . '/dev/dump.php';
-$config['sesto_error_extended_info'] = true;
